95ec664e9c21ea3f15084e8a323c1dd9af81348f
kennja05 <jacobkenny05@gmail.com>
2020-02-21 17:40:28 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,39 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleChange = (event) => {
+    // event.preventDefault() - does not satisfy the test requirement to use preventDefault() upon form submission
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    if (!this.state.username || !this.state.password) return
+    this.props.handleLogin(this.state)
   }
 
   render() {
+    // console.log(this.state.username, this.state.password) - username and password states are updated with each change to input field
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={event => this.handleChange(event)} value={this.state.username} id="username" name="username" type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={event => this.handleChange(event)} value={this.state.password} id="password" name="password" type="password" />
           </label>
         </div>
         <div>
